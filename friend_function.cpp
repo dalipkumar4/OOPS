19ba5da4d9e074c99a2fe3e9e5b79e41a58434c8
Dalip-Kumar <dalipkumar3559@gmail.com>
2025-10-22 08:42:14 +0530

    Add Patient class with friend function for records
    Implement a Patient class with a friend function to display patient details.

---
+++ friend_function.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+class Patient {
+    private:
+        string name;
+        int age;
+        string disease;
+    public: 
+        // Constructor to intialize details
+        Patient(string n,int a,string d) {
+            name = n;
+            age = a;
+            disease = d;
+        }
+
+        // Dedlaring friend function
+        friend void showRecord(Patient p);
+};
+
+void showRecord(Patient p){
+    cout << "Docto is checking patient record...\n";
+    cout << "Name: " << p.name << endl;
+    cout << "Age: " << p.age << endl;
+    cout << "Disease: " << p.disease << endl;
+    
+}
+
+int main(){
+    Patient p1("Krish", 20, "Alzheimers");
+    showRecord(p1); // Friend funcction can acess private data
+
+    return 0;
+}
